779588255625341bc963713bbf02541c0bc6f7a9
datashegz <olusegun.fagbamiye@gmail.com>
2022-07-20 17:20:41 +0000

    Removed bucket prefix from module definition

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/HarshiCorpWorkShop/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  bucket_prefix = "Segun"
+  bucket = "segun-my-s3-bucket"
   acl    = "private"
 
   versioning = {
